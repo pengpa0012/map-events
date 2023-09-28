0ae0506b14b065418e75dea8cab1ff4eb4016ffa
Pengpa0012 <peng.ilikha@gmail.com>
2023-09-28 22:32:05 +0800

    update dropzone

--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -1,16 +1,23 @@
-import { Input, Textarea, Group, Text, rem, Button } from '@mantine/core'
+import { Input, Textarea, Group, Text, rem, Button, Image } from '@mantine/core'
 import dynamic from 'next/dynamic'
-import React from 'react'
+import React, { useState } from 'react'
 import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone'
 const Map = dynamic(() => import('../../components/map'), { ssr: false })
 
 export default function report() {
+  const [images, setImages] = useState<File[]>([])
+  const [previewImages, setPreviewImages] = useState<string[]>([])
+
   return (
     <div className="pb-20">
       <Map report />
       <Dropzone
-        onDrop={(files) => console.log('accepted files', files)}
-        onReject={(files) => console.log('rejected files', files)}
+        multiple
+        onDrop={(files) => {
+          const blob = window.URL.createObjectURL(files[0])
+          setPreviewImages((prev) => [...prev, blob])
+          setImages((prev) => [...prev, files[0]])
+        }}
         maxSize={3 * 1024 ** 2}
         accept={IMAGE_MIME_TYPE}
         className="border border-gray-100"
@@ -21,16 +28,22 @@ export default function report() {
           mih={220}
           style={{ pointerEvents: 'none' }}
         >
-          <Dropzone.Accept>
-            <h1>IMAGE UPLOADED</h1>
-          </Dropzone.Accept>
           <div>
-            <Text size="xl" inline>
-              Drag images here or click to select files
-            </Text>
-            <Text size="sm" c="dimmed" inline mt={7}>
-              Attach as many files as you like, each file should not exceed 5mb
-            </Text>
+            {images.length <= 0 ? (
+              <Text size="xl" inline>
+                Drag images here or click to select files
+              </Text>
+            ) : (
+              <div className="flex flex-wrap gap-2">
+                {previewImages.map((img, i) => (
+                  <Image
+                    src={img}
+                    key={`img-${i}`}
+                    className="w-[200px] h-[200px]"
+                  />
+                ))}
+              </div>
+            )}
           </div>
         </Group>
       </Dropzone>
